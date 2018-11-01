bc80eb51151431c688916bb0c7d1838dd265775c
nicoptv <nicoptv@gmail.com>
2018-11-01 12:38:45 -0400

    first commit

---
+++ src/app/deck/deck.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deck',
+  templateUrl: './deck.component.html',
+  styleUrls: ['./deck.component.css']
+})
+export class DeckComponent implements OnInit {
+
+  deck : string[] = [];
+  url : string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initCard();
+  }
+
+  shuffle () : void{
+    let idCard : number;
+    for (let i = this.deck.length - 1; i > 0; i--) {
+      idCard = Math.floor(Math.random() * (i + 1));
+      [this.deck[i], this.deck[idCard]] = [this.deck[idCard], this.deck[i]]
+    }
+  }
+
+  dealOneCard () : void {
+    if (this.deck.length != 0){
+      this.url = this.deck[0];
+      this.deck.splice(0,1);
+    }else{
+      this.url = undefined;
+      console.log("jeu de carte vide");
+    }
+  }
+
+  initCard() : void{
+    for (let index = 1; index < 53; index++) {
+      this.deck.push('../../assets/' + index + '.jpg');
+    }
+  }
+}
